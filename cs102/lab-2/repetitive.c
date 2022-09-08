7d9090a04b437c2970189b0af911ebfda41e3b05
Mariano Vinas <mariano.vinas@cooper.edu>
2022-09-07 20:26:48 -0400

    lab 2 stuff

--- cs102/lab-2/repetitive.c
+++ cs102/lab-2/repetitive.c
@@ -2,27 +2,23 @@
 int main( int argc, char**argv )
 {
 
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "hello world!\n");
-fprintf(stdout, "intermission\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
-fprintf(stdout, "goodbye\n");
+int i = 0;
+
+while (i < 50)
+{
+fprintf(stdout, "step up\n");
+fprintf(stdout, "step down\n");
+i = i + 1;
+}
+
+fprintf(stdout, "rest for a minute\n");
+i = 0;
+
+while (i < 50){
+fprintf(stdout, "step up\n");
+fprintf(stdout, "step down\n");
+i = i + 1;
+}
 
 return 0;
 }
