4584aa39eb577e910f80508a53249fc314cdc559
haticebezci <hatice.bezci@cooper.edu>
2022-09-14 20:59:02 -0400

    graph added to hw folder

---
+++ cs102/homework-3/graph.c
@@ -0,0 +1,27 @@
+/* Print multiples of 13, and more, and a sum of multiples */
+#include <stdio.h>
+int main( int argc, char **argv )
+{
+int A = 7;
+int B = 5;
+int C = 8;
+int D = 4;
+
+int x = -100;
+int y = 0;
+int h = D;
+int yprime = 0;
+int yint = 0;
+int ysum = 0;
+fprintf( stdout, "x;y;yprime;ysum\n");
+while( x<= 100)
+{
+y = A*x*x*x - B*x*x + C*x;
+yprime = ( (A*x*x*x - B*x*x + C*x) - ( A*(x-h)*(x-h)*(x-h) - B*(x-h)*(x-h) + C*(x-h) ) )/h;
+yint = (h * ((A*x*x*x - B*x*x + C*x) + ( A*(x-h)*(x-h)*(x-h) - B*(x-h)*(x-h) + C*(x-h) ) )/2);
+ysum = ysum + yint;
+fprintf( stdout, "%d; %d; %d; %d \n", x, y, yprime, ysum);
+x = x + D;
+}
+return 0;
+}
